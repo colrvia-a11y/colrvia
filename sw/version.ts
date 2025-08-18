d33e60da5cfe384e6e9dd1b728e876173e3a39e2
colrvia-a11y <colrvia@gmail.com>
2025-08-17 22:39:44 -0500

    Delete sw directory

--- sw/version.ts
+++
@@ -1,8 +0,0 @@
-/// <reference lib="webworker" />
-export const VERSION = 'v2';
-export const CORE_URLS = ['/', '/offline'];
-
-export async function precacheCore() {
-  const cache = await caches.open(VERSION);
-  await cache.addAll(CORE_URLS);
-}
